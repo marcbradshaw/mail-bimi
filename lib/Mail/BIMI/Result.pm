0e2d6d4482f1a5102edf83207690746a20c1694d
Marc Bradshaw <marc@marcbradshaw.net>
2018-02-09 12:49:17 +1100

    Use Mail::AuthenticationResults

--- lib/Mail/BIMI/Result.pm
+++ lib/Mail/BIMI/Result.pm
@@ -8,6 +8,10 @@ use warnings;
 use Carp;
 use English qw( -no_match_vars );
 
+use Mail::AuthenticationResults::Header::Entry;
+use Mail::AuthenticationResults::Header::SubEntry;
+use Mail::AuthenticationResults::Header::Comment;
+
 sub new {
     my ( $Class ) = @_;
     my $Self = {
@@ -45,14 +49,22 @@ sub result {
     return $Self->{ 'result' };
 }
 
+sub get_authentication_results_object {
+    my ( $Self ) = @_;
+    my $header = Mail::AuthenticationResults::Header::Entry->new()->set_key( 'bimi' )->safe_set_value( $Self->{ 'result' } );
+    if ( $Self->{ 'result_comment' } ) {
+        $header->add_child( Mail::AuthenticationResults::Header::Comment->new()->safe_set_value( $Self->{ 'result_comment' } ) );
+    }
+    if ( $Self->{ 'result' } eq 'pass' ) {
+        $header->add_child( Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'header.d' )->safe_set_value( $Self->{ 'domain' } ) );
+        $header->add_child( Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'selector' )->safe_set_value( $Self->{ 'selector' } ) );
+    }
+    return $header;
+}
+
 sub get_authentication_results {
     my ( $Self ) = @_;
-    my @Result;
-    push @Result, 'bimi=' . $Self->{ 'result' };
-    push @Result, '(' . $Self->{ 'result_comment' } . ')' if $Self->{ 'result_comment' };
-    push @Result, 'header.d=' . $Self->{ 'domain' } if $Self->{ 'result' } eq 'pass';
-    push @Result, 'selector=' . $Self->{ 'selector' } if $Self->{ 'result' } eq 'pass';
-    return join( ' ', @Result );
+    return $Self->get_authentication_results_object->as_string();
 }
 
 sub get_bimi_location {
