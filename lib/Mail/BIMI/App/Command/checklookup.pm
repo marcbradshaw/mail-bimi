9bad6d9fef73f87dd6b86250b97e7374aaf19582
Marc Bradshaw <marc@marcbradshaw.net>
2020-05-20 06:45:47 +0000

    Command to just to a lookup

---
+++ lib/Mail/BIMI/App/Command/checklookup.pm
@@ -0,0 +1,59 @@
+package Mail::BIMI::App::Command::checklookup;
+# ABSTRACT: Check a domain for BIMI
+# VERSION
+use 5.20.0;
+BEGIN { $ENV{MAIL_BIMI_CACHE_BACKEND} = 'Null' };
+use Mail::BIMI::Pragmas;
+use Mail::BIMI::App -command;
+use Mail::BIMI;
+use Mail::BIMI::Record;
+use Mail::DMARC;
+
+=head1 DESCRIPTION
+
+App::Cmd class implementing the 'mailbimi checkrecord' command
+
+=cut
+
+sub abstract { 'Validate a given BIMI domain' }
+sub description { 'Mail::BIMI domain validator' };
+sub usage_desc { "%c checklookup %o <DOMAIN>" }
+
+sub opt_spec {
+  return (
+    [ 'selector=s', 'Optional selector' ],
+  );
+}
+
+sub validate_args($self,$opt,$args) {
+ $self->usage_error('No Domain specified') if !@$args;
+ $self->usage_error('Multiple Domains specified') if scalar @$args > 1;
+
+}
+
+sub execute($self,$opt,$args) {
+  my $domain = $args->[0];
+  my $selector = $opt->selector // 'default';
+
+  my $dmarc = Mail::DMARC::PurePerl->new;
+  $dmarc->result()->result( 'pass' );
+  $dmarc->result()->disposition( 'reject' );
+  my $bimi = Mail::BIMI->new(
+    dmarc_object => $dmarc->result,
+    domain => $domain,
+    selector => $selector,
+  );
+
+  say 'An authenticated email with this record would receive the following BIMI results:';
+  say '';
+  my $result = $bimi->result;
+  say "Authentication-Reults: authservid.example.com; ".$result->get_authentication_results;
+  my $headers = $result->headers;
+  foreach my $header ( sort keys $headers->%* ) {
+      say "$header: ".$headers->{$header};
+  }
+
+}
+
+1;
+
