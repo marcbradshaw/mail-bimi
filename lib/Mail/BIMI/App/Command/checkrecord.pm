637c62a7e7a1fa0476d4b9d418b67e15053dca72
Marc Bradshaw <marc@marcbradshaw.net>
2020-08-04 02:35:36 +0000

    Command for checking records

---
+++ lib/Mail/BIMI/App/Command/checkrecord.pm
@@ -0,0 +1,84 @@
+package Mail::BIMI::App::Command::checkrecord;
+# ABSTRACT: Validate a given BIMI assertion record
+# VERSION
+use 5.20.0;
+BEGIN { $ENV{MAIL_BIMI_CACHE_DEFAULT_BACKEND} = 'Null' };
+use Mail::BIMI::Pragmas;
+use Mail::BIMI::App -command;
+use Mail::BIMI;
+use Mail::BIMI::Record;
+use Mail::DMARC;
+use Term::ANSIColor qw{ :constants };
+
+=head1 DESCRIPTION
+
+App::Cmd class implementing the 'mailbimi checkdomain' command
+
+=cut
+
+sub description { 'Check a given BIMI assertion record' };
+sub usage_desc { "%c checkrecord %o <RECORD>" }
+
+sub opt_spec {
+  return (
+    [ 'domain=s', 'Optional domain' ],
+  );
+}
+
+sub validate_args($self,$opt,$args) {
+ $self->usage_error('No Record specified') if !@$args;
+ $self->usage_error('Multiple Records specified') if scalar @$args > 1;
+}
+
+sub execute($self,$opt,$args) {
+  my $check_record = $args->[0];
+  my $domain = $opt->domain // '-none-';
+
+  my $dmarc = Mail::DMARC::PurePerl->new;
+  $dmarc->header_from($opt->domain // 'reject.test.fastmaildmarc.com');
+  $dmarc->validate;
+  $dmarc->result->result('pass');
+  my $bimi = Mail::BIMI->new(
+    dmarc_object => $dmarc,
+    domain => $domain,
+    OPT_FORCE_RECORD => $check_record,
+    OPT_VMC_NO_CHECK_ALT => $opt->domain ? 0 : 1,
+  );
+
+  my $record = $bimi->record;
+  #  my $record = Mail::BIMI::Record->new( domain => $domain, selector => $selector );
+  say "BIMI domain checker";
+  say '';
+  say 'Requested:';
+  say YELLOW.'  Domain    '.WHITE.': '.CYAN.$domain.RESET;
+  say YELLOW.'  Record    '.WHITE.': '.CYAN.$check_record.RESET;
+  say '';
+  $record->app_validate;
+  if ( $record->location && $record->location->indicator ) {
+    say '';
+    $record->location->indicator->app_validate;
+  }
+  if ( $record->authority && $record->authority->vmc ) {
+    say '';
+    $record->authority->vmc->app_validate;
+    if ( $record->authority->vmc->indicator ) {
+        say '';
+        $record->authority->vmc->indicator->app_validate;
+    }
+  }
+  say '';
+
+  say 'An authenticated email with this record would receive the following BIMI results:';
+  say '';
+  my $result = $bimi->result;
+  say "Authentication-Reults: authservid.example.com; ".$result->get_authentication_results;
+  my $headers = $result->headers;
+  foreach my $header ( sort keys $headers->%* ) {
+      say "$header: ".$headers->{$header};
+  }
+
+  $bimi->finish;
+}
+
+1;
+
